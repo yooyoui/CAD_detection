afbdef2b36c06343c82c8ef45144bc3749e784de
Violet <qq3317993@outlook.com>
2024-07-09 15:56:55 +0800

    修改了src

--- src/main/java/com/chan/grpc_callpy/service/IScriptExecute.java
+++
@@ -1,8 +0,0 @@
-package com.chan.grpc_callpy.service;
-
-import com.chan.grpc_callpy.entity.ScriptContent;
-import com.chan.grpc_callpy.entity.ScriptExecResult;
-
-public interface IScriptExecute {
-    ScriptExecResult exec(ScriptContent content);
-}
